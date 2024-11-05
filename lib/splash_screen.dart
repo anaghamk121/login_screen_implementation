dfbb5615bfbfbca6e3cb6775abb84fb49df94e6a
Anaghamk00 <anaghadev00@gmail.com>
2024-11-05 23:11:09 +0530

    second commit splash screen

---
+++ lib/splash_screen.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class Splash extends StatefulWidget {
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  void initState() {
+    Timer(const Duration(seconds: 5), () {
+      // Navigator.pushReplacement(
+      //     context,
+      //     MaterialPageRoute(builder: (context) => MyApp()));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SvgPicture.asset("asset/Layer_1image.svg",height: 100,width: 100,),
+      ),
+    );
+  }
+}
